f90cb478b30baa2c9d11b1642f4d0ecf7f734a50
Kevinho-jp <mukendikevin668@gmail.com>
2026-04-21 09:55:31 +0200

    Remove PayPal and fix boutique page

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Désactive la vérification TypeScript lors du build
     ignoreBuildErrors: true,
   },
 };
